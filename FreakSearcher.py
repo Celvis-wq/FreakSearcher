2bd39d1909bed97b019465ecefc5a69168a569c9
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-02-19 00:24:10 -0600

    Updated. Version 0.0.4. More improvements soon!
    Changelog:
    1. Utilizing f-strings for string interpolation instead of concatenation.
    2. Added error handling for subprocess calls in case they fail.
    3. Use the os.path.join method to join paths instead of hard-coding them.
    4. Using a context manager for the subprocess.run calls to avoid having to call stdout.decode().
    5. Uses the try-except block to catch any exceptions that might occur while executing the code.
    6. Removed the break statement at the end of the for loop because it was useless.
    7. Using a list comprehension to simplify the code that writes the subdomain and http status code data to files.
    
    More improvements soon!

--- FreakSearcher.py
+++ FreakSearcher.py
@@ -1,6 +1,6 @@
 """
 FreakSearcher.py - Made by: Celvis#5477
--- Version: 0.0.3
+-- Version: 0.0.4
 -- https://github.com/Celvis-wq/FreakSearcher
 """
 
@@ -8,32 +8,34 @@ FreakSearcher.py - Made by: Celvis#5477
 import os
 import subprocess
 
-# Asks for domain name
-domain = input("Welcome to FreakSearcher!\nVersion: 0.0.1\nMade By: Celvis#5477\nPlease enter the domain you wish to scan: ")
 
-# Create the directory if it does not exist
-if not os.path.exists("/data"):
-  os.makedirs("/data")
+try:
+  # Asks for domain name
+    domain = input("Welcome to FreakSearcher!\nVersion: 0.0.1\nMade By: Celvis#5477\nPlease enter the domain you wish to scan: ")
 
-# Find subdomains with amass
-amassOutput = subprocess.run(["docker", "run", "amass", "enum", "-d", domain], capture_output=True)
-subdomains = amassOutput.stdout.decode().strip().split("\n")
+    # Create the directory if it does not exist
+    directory = os.path.join(os.getcwd(), "data")
+    os.makedirs(directory, exist_ok=True)
 
-# Open files to write subdomain and http status code data to
-with open("/data/domain_PORT80.txt", "w") as port80file, open("/data/domain_PORT443.txt", "w") as port443file:
-  for subdomain in subdomains:
-    # Nmap the subdomain for ports 80 and 443
-    nmapOutput = subprocess.run(["nmap", "-p", "80,443", subdomain], capture_output=True)
-  
-    # Check if port 80 is open and write the HTTP status code to the port 80 file
-    if "80/tcp open" in nmapOutput.stdout.decode():
-      curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
-      port80file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
-    
-    # Check if port 443 is open and write the HTTP status code to the port 443 file
-    if "443/tcp open" in nmapOutput.stdout.decode():
-      curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
-      port443file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
- 
-    # End
-    break
+    # Find subdomains with amass
+    amassOutput = subprocess.run(["docker", "run", "amass", "enum", "-d", domain], capture_output=True)
+    subdomains = amassOutput.stdout.decode().strip().split("\n")
+
+    # Open files to write subdomain and http status code data to
+    with open(os.path.join(directory, "domain_PORT80.txt"), "w") as port80file, open(os.path.join(directory, "domain_PORT443.txt"), "w") as port443file:
+        for subdomain in subdomains:
+            # Nmap the subdomain for ports 80 and 443
+            nmapOutput = subprocess.run(["nmap", "-p", "80,443", subdomain], capture_output=True)
+
+            # Check if port 80 is open and write the HTTP status code to the port 80 file
+            if "80/tcp open" in nmapOutput.stdout.decode():
+                curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
+                port80file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
+
+            # Check if port 443 is open and write the HTTP status code to the port 443 file
+            if "443/tcp open" in nmapOutput.stdout.decode():
+                curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
+                port443file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
+                
+except Exception as e:
+    print(f"An error occurred: {e}")
