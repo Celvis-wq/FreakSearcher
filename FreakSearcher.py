308924a42d6268a96b633fae955db04ad6ff9016
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-08-23 20:09:52 -0500

    Update FreakSearcher.py
    Updated freaksearcher. Version: 0.0.5

--- FreakSearcher.py
+++ FreakSearcher.py
@@ -1,6 +1,6 @@
 """
-FreakSearcher.py - Made by: Celvis#5477
--- Version: 0.0.4
+FreakSearcher.py - Made by: Celvis - Discord: Celvis
+-- Version: 0.0.5
 -- https://github.com/Celvis-wq/FreakSearcher
 """
 
@@ -8,34 +8,44 @@ FreakSearcher.py - Made by: Celvis#5477
 import os
 import subprocess
 
+# Fetch the headers of an HTTP response from a given subdomain
+def getHttpStatus(subdomain):
+    curlOutput = subprocess.run(["curl", "-I", subdomain], captureOutput=True, text=True)
+    httpHeaders = curlOutput.stdout.split('\n')
+    statusLine = [line for line in httpHeaders if line.startswith("HTTP/")]
+    if statusLine:
+        return statusLine[0].split()[1]
+    return "Unknown"
 
 try:
-  # Asks for domain name
-    domain = input("Welcome to FreakSearcher!\nVersion: 0.0.1\nMade By: Celvis#5477\nPlease enter the domain you wish to scan: ")
+    # Asks for domain name
+    domain = input("Welcome to FreakSearcher!\nVersion: 0.0.5\nMade By: Celvis\nDiscord: Celvis\nPlease enter the domain you wish to scan: ")
 
     # Create the directory if it does not exist
     directory = os.path.join(os.getcwd(), "data")
-    os.makedirs(directory, exist_ok=True)
+    os.makedirs(directory, existOk=True)
 
     # Find subdomains with amass
-    amassOutput = subprocess.run(["docker", "run", "amass", "enum", "-d", domain], capture_output=True)
-    subdomains = amassOutput.stdout.decode().strip().split("\n")
+    amassOutput = subprocess.run(["docker", "run", "amass", "enum", "-d", domain], capture_output=True, text=True)
+    subdomains = amassOutput.stdout.strip().split("\n")
 
     # Open files to write subdomain and http status code data to
-    with open(os.path.join(directory, "domain_PORT80.txt"), "w") as port80file, open(os.path.join(directory, "domain_PORT443.txt"), "w") as port443file:
+    with open(os.path.join(directory, "Port80.txt"), "w") as port80file, open(os.path.join(directory, "Port443.txt"), "w") as port443file:
         for subdomain in subdomains:
             # Nmap the subdomain for ports 80 and 443
-            nmapOutput = subprocess.run(["nmap", "-p", "80,443", subdomain], capture_output=True)
+            nmapOutput = subprocess.run(["nmap", "-p", "80,443", subdomain], capture_output=True, text=True)
+
+            nmapOutputStr = nmapOutput.stdout
 
             # Check if port 80 is open and write the HTTP status code to the port 80 file
-            if "80/tcp open" in nmapOutput.stdout.decode():
-                curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
-                port80file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
+            if "80/tcp open" in nmapOutputStr:
+                httpStatus = getHttpStatus(subdomain)
+                port80file.write(f"{subdomain}: {httpStatus}\n")
 
             # Check if port 443 is open and write the HTTP status code to the port 443 file
-            if "443/tcp open" in nmapOutput.stdout.decode():
-                curl_output = subprocess.run(["curl", "-I", subdomain], capture_output=True)
-                port443file.write(f"{subdomain}: {curl_output.stdout.decode().split()[1]}\n")
-                
+            if "443/tcp open" in nmapOutputStr:
+                httpStatus = getHttpStatus(subdomain)
+                port443file.write(f"{subdomain}: {httpStatus}\n")
+
 except Exception as e:
     print(f"An error occurred: {e}")
