a817edabd8baa042b9453b3e5045103ec0aacc82
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-11-15 11:11:52 -0600

    Update FreakSearcher.py

--- FreakSearcher.py
+++ FreakSearcher.py
@@ -6,6 +6,7 @@ FreakSearcher.py - Made by: Celvis - Discord: Celvis
 -- bug fixes soon
 """
 
+# Import
 import os
 import requests
 import nmap
@@ -33,42 +34,40 @@ def checkRequiredTools():
         exit(1)
 
 try:
-    # Check for required tools
     checkRequiredTools()
 
-    # Asks for domain name
-    domain = input("Welcome to FreakSearcher!\nVersion: 0.0.7\nMade By: Celvis\nDiscord: Celvis\nPlease enter the domain you wish to scan: ")
+    domain = input("Welcome to FreakSearcher! | Version: 0.0.7 | Made By: Celvis | Discord: Celvis\n\nPlease enter the domain you wish to scan: ")
 
-    # Create the directory if it does not exist
     directory = os.path.join(os.getcwd(), "data")
     try:
         os.makedirs(directory)
     except FileExistsError:
         pass
 
-    # Find subdomains with amass
     amassOutput = subprocess.run(["docker", "run", "amass", "enum", "-d", domain], capture_output=True, text=True)
-    subdomains = amassOutput.stdout.strip().split("\n")
+    subdomains = [sd.strip() for sd in amassOutput.stdout.strip().split("\n") if sd.strip()]
 
-    # Initialize Nmap Scanner
     nmScanner = nmap.PortScanner()
 
-    # Open files to write subdomain and HTTP status code data to
     with open(os.path.join(directory, "Port80.txt"), "w") as port80File, open(os.path.join(directory, "Port443.txt"), "w") as port443File:
         for subdomain in subdomains:
-            # Nmap the subdomain for ports 80 and 443
+            print("Scanning subdomain:", subdomain)
+
             nmScanner.scan(subdomain, '80,443')
 
-            # Check if port 80 is open and write the HTTP status code to the port 80 file
+            if subdomain not in nmScanner.all_hosts():
+                print(f"Subdomain {subdomain} not found in scan results.")
+                continue
+
             if nmScanner[subdomain].has_tcp(80) and nmScanner[subdomain]['tcp'][80]['state'] == 'open':
                 httpStatus = getHttpStatus(subdomain)
                 port80File.write(f"{subdomain}: {httpStatus}\n")
 
-            # Check if port 443 is open and write the HTTP status code to the port 443 file
             if nmScanner[subdomain].has_tcp(443) and nmScanner[subdomain]['tcp'][443]['state'] == 'open':
                 httpStatus = getHttpStatus(subdomain)
                 port443File.write(f"{subdomain}: {httpStatus}\n")
 
 except Exception as e:
     print(f"An error occurred: {e}")
-    traceback.print_exc() 
+    traceback.print_exc()
+
